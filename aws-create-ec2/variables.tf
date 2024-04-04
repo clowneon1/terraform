532fef484266cae960aab1fb0c3bd1242a3c1dfe
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-04 20:22:23 +0530

    add variable defaults

--- aws-create-ec2/variables.tf
+++ aws-create-ec2/variables.tf
@@ -1,9 +1,11 @@
 variable "instance_type" {
-  type = string
+  type    = string
+  default = "t2.micro"
 }
 
 variable "image_ami" {
-  type = string
+  default = "ami-0187337106779cdf8" #variable defaults is used if no specific value is defined in .tfvars file
+  type    = string
 }
 
 #create terraform.tfvars and use it to define the values

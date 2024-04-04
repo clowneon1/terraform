6e14f76df6a027178667e51a4b562a0b7ef23a7d
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-04 20:00:30 +0530

    add variables.tf file

--- aws-create-ec2/aws.tf
+++ aws-create-ec2/aws.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "serverbyt" {
-  ami           = "ami-0187337106779cdf8"
-  instance_type = "t2.micro"
+  ami           = var.image_ami
+  instance_type = var.instance_type
 
   tags = {
     Name = "serverbyt"
---
+++ aws-create-ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "image_ami" {
+  type = string
+}
+
+#create terraform.tfvars and use it to define the values

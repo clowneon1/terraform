c6042fb95db7da09333b94c437ecaba2c02975ea
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-09 20:31:06 +0530

    use list and map variable

--- aws-create-ec2/aws.tf
+++ aws-create-ec2/aws.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "serverbyt" {
   ami           = var.image_ami
-  instance_type = var.instance_type
+  instance_type = var.instance_types["micro"]
+  # instance_type = var.list[0]
 
   tags = {
     Name = "serverbyt"
--- aws-create-ec2/variables.tf
+++ aws-create-ec2/variables.tf
@@ -8,4 +8,18 @@ variable "image_ami" {
   type    = string
 }
 
+variable "list" {
+  type    = list(string)
+  default = ["m5.large", "m5.xlarge", "t2.medium"]
+}
+
+variable "instance_types" {
+  type = map(any)
+  default = {
+    micro = "t2.micro"
+    nano  = "t2.nano"
+    small = "t2.small"
+  }
+}
+
 #create terraform.tfvars and use it to define the values

0c4f5bbaed005fda95b22f9580cf841c08557eaf
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-16 20:19:47 +0530

    use data source to get the ami

--- aws-create-ec2/aws.tf
+++ aws-create-ec2/aws.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "serverbyt" {
-  ami           = var.image_ami
+  # ami           = var.image_ami
+  # using data source to get the ami
+  ami = data.aws_ami.instance_ami.id
+
   # instance_type = var.instance_type_list[count.index]
   instance_type = var.instance_types["micro"]
   # instance_type = var.list[0]
@@ -8,6 +11,15 @@ resource "aws_instance" "serverbyt" {
     name = "serverbyt-${var.instance_type_list[count.index]}"
   }
   # this will create 2 ec2 instances 
-  count = 2 #count starts from 0, terraform specific command.
+  count = 1#count starts from 0, terraform specific command.
 }
 
+data "aws_ami" "instance_ami" {
+  most_recent = true
+  owners = [ "amazon" ]
+
+  filter {
+    name = "name"
+    values = [ "amzn2-ami-hvm*" ]
+  }
+}

433599baffa90170529059f4d6c723f1414f5f02
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-16 18:16:35 +0530

    introduction to terraform functions

---
+++ terrafrom-functions/functions.tf
@@ -0,0 +1,25 @@
+locals {
+  time = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
+}
+
+resource "aws_key_pair" "loginkey" {
+  key_name   = "login-key"
+  public_key = file("${path.module}/id_rsa.pub")
+#   use valid rsa file 
+}
+
+resource "aws_instance" "app-dev" {
+  ami           = lookup(var.ami, var.region)
+  instance_type = "t2.micro"
+     key_name = aws_key_pair.loginkey.key_name
+  count = 1
+
+  tags = {
+    Name = element(var.tags, count.index)
+  }
+}
+
+
+output "timestamp" {
+  value = local.time
+}

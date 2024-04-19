de47eb83af8fed52e0f18c36831a934e51c4bec5
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-20 03:16:18 +0530

    use local-exec

--- terraform-provisioners/provisioner.tf
+++ terraform-provisioners/provisioner.tf
@@ -1,23 +1,24 @@
-resource "aws_instance" "tfserver" {
-  ami           = var.image_ami
-  instance_type = var.instance_type
-  key_name      = "terraform"
+# resource "aws_instance" "tfserver" {
+#   ami           = var.image_ami
+#   instance_type = var.instance_type
+#   key_name      = "terraform"
 
-#   security_groups = [ "launch-wizard-1" , "default" ]
-  vpc_security_group_ids = [ "sg-03040ce6c8e99ab47" ]
+# #   security_groups = [ "launch-wizard-1" , "default" ]
+#   vpc_security_group_ids = [ "sg-03040ce6c8e99ab47" ]
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras install -y nginx1.12",
-      "sudo systemctl start nginx"
-    ]
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file(var.private_key_path)
-    }
-  }
-}
+# connection {
+#       type        = "ssh"
+#       host        = self.public_ip
+#       user        = "ec2-user"
+#       private_key = file(var.private_key_path)
+#     }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo amazon-linux-extras install -y nginx1.12",
+#       "sudo systemctl start nginx"
+#     ]
+    
+#   }
+# }
 
 

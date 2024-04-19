138687f802b1ec9f60326b034388753471536d88
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-19 18:21:00 +0530

    use zipmap function

--- aws-create-iam/splat.tf
+++ aws-create-iam/splat.tf
@@ -10,3 +10,11 @@ output "iam_username" {
 output "arns" {
   value = aws_iam_user.lb[*].arn #that star is a splat expression
 }
+
+output "username_arn_map" {
+  value = zipmap(aws_iam_user.lb[*].name, aws_iam_user.lb[*].arn)
+}
+
+#zipmap is used to create a map from two lists.
+
+# // /* */ these three comments can be used for this.

975eb209922472b023fcbf4444d2f33361cfcc2f
clowneon1 <er.mandloiyashraj@gmail.com>
2024-04-22 03:09:45 +0530

    tacking config change problem with modules

--- terraform-modules/modules/ec2/ec2.tf
+++ terraform-modules/modules/ec2/ec2.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "server_from_module" {
-    ami = var.image_ami
-    instance_type = "t2.micro"
+    ami = "ami-0187337106779cdf8" # not making it variable so that person using the module can't change it
+    instance_type = var.instance_type
     security_groups = [ "launch-wizard-1"]
     tags = {
       Name = "server-from-module"
     }
 }
+
+# using variables lets you change the configuration as where 
+# we are using the module we have no control over the module configurations.

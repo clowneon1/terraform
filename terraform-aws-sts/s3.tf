1dbf60f34452dffef980a3c45d75d5853d8c682e
Yashraj Mandloi <er.mandloiyashraj@gmail.com>
2024-05-16 22:19:14 +0530

    use sensitive arrtribue with outputs

--- terraform-aws-sts/s3.tf
+++ terraform-aws-sts/s3.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket        = "tf-buc"
   force_destroy = true
+  tags          = local.tags
+}
+
+locals {
   tags = {
     name = "terraform bucket"
     env  = "dev"
   }
 }
+
+output "bucket_arn" {
+  value     = aws_s3_bucket.s3_bucket.arn
+  sensitive = true #this will prevent the info from getting logged in console and terraform cloud
+}
+# the value is still in plain sight in .tfstate file though.

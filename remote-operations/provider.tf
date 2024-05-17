6d21229a4b922a340b2877c31dfba58296aea9f6
Yashraj Mandloi <er.mandloiyashraj@gmail.com>
2024-05-17 17:21:04 +0530

    add comment regarding state file

--- remote-operations/provider.tf
+++ remote-operations/provider.tf
@@ -10,7 +10,7 @@ terraform {
   # this is one way(new way) of doing it
   # make sure to configute the terraform token using terraform login command
   # also create the appropriate org and workspace
-
+  # for remote operations also the state file will be stored on terraform cloud.
   cloud {
     organization = "remote-org"
 
